b649bed152e13c59a318e795ca54b5b960b1bce0
LeTengu <letengu@outlook.fr>
2019-03-26 19:34:24 +0100

    7

--- test_main.py
+++ test_main.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+# encoding: utf-8
+"""
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print('Connected')
+    mqtt.surscribe('hermes/intent/#')
+
+mqtt = mqtt.Client()
+mqtt.on_connect = on_connect
+mqtt.connect('raspberry,local', 1883)
+mqtt.loop_forever()
+"""
+
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import MqttOptions
 
@@ -9,12 +23,12 @@ class AddNumber(snips_common.ActionWrapper):
     def action(self):
         number_1 = self.intent_message.slots.nb_1.first().value
         number_2 = self.intent_message.slots.nb_2.first().value
-        result = nb_1 * nb_2
+        result = nb_1 + nb_2
         self.end_session("Le résultat de l'addition est ",result)
 
 if __name__ == '__main__':
     mqtt_opts = MqttOptions()
 
-    with Hermes(mqtt_options=mqtt_opts) as h:
+    with Hermes('raspberrypi.local:1883') as h:
         h.subscribe_intent("Tengu:addNumber", AddNumber.callback).start()
 
