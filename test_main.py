32461ebe981449cfb7563ebad190499c3d049baf
LeTengu <letengu@outlook.fr>
2019-03-27 16:53:52 +0100

    8 jje crois !

--- test_main.py
+++ test_main.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python3
-# encoding: utf-8
-"""
-import paho.mqtt.client as mqtt
+# -*- coding: utf-8 -*-
 
-def on_connect(client, userdata, flags, rc):
-    print('Connected')
-    mqtt.surscribe('hermes/intent/#')
-
-mqtt = mqtt.Client()
-mqtt.on_connect = on_connect
-mqtt.connect('raspberry,local', 1883)
-mqtt.loop_forever()
-"""
-
+from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
-from hermes_python.ontology import MqttOptions
+from hermes_python.ontology import *
+import io
 
-import snips_common
+CONFIG_INI = "config.ini"
 
-class AddNumber(snips_common.ActionWrapper):
-    
-    def action(self):
-        number_1 = self.intent_message.slots.nb_1.first().value
-        number_2 = self.intent_message.slots.nb_2.first().value
-        result = nb_1 + nb_2
-        self.end_session("Le résultat de l'addition est ",result)
+class Calcul(object):
+
+    def __init__(self):
+        try:
+            self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
+        except :
+            self.config = None
+
+        # start listen MQTT
+        self.start_blocking()
+
+
+    def addNumber_callback(self, hermes, intent_message):
+        # if not continue, terminate session
+        hermes.publish_end_session(intent_message.session_id, "Pas possible Monsieur !")
+
+        # action code
+        if intent_message.slots.nb_1 and intent_message.slots.nb_2:
+            nb_1 = intent_message.slots.nb_1.first().value()
+            nb_2 = intent_message.slots.nb_2.first().value()
+            result = int(nb_1 + nb_2)
+
+        # audio return
+        hermes.publish_start_session_notification(intent_message.site_id, "Le résultat est de", result)
+
+
+    def master_intent_callback(self, hermes, intent_message):
+        coming_intent = intent_message.intent.intent_name
+        if coming_intent == 'addNumber':
+            self.addNumber_callback(hermes, intent_message)
+
+
+    # register callback function and start MQTT
+    def start_blocking(self):
+        with Hermes(MQTT_ADDR) as h:
+            h.subcribe_intents(self.master_intent_callback).start()
 
 if __name__ == '__main__':
-    mqtt_opts = MqttOptions()
-
-    with Hermes('raspberrypi.local:1883') as h:
-        h.subscribe_intent("Tengu:addNumber", AddNumber.callback).start()
-
+    Calcul()
