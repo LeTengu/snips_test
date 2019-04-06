57189118713cda5684d919fb58a3c675ab687447
LeTengu <letengu@outlook.fr>
2019-04-06 20:09:35 +0200

    b

--- test_main.py
+++ test_main.py
@@ -3,39 +3,28 @@
 
 from hermes_python.hermes import Hermes
 
-MQTT_IP_ADDR = "localhost"
+MQTT_IP_ADDR = "raspi-tengu"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
 def intent_received(hermes, intent_message):
-    sentence = 'You asked for '
+    sentence = 'La réponse : '
 
-    if intent_message.intent.intent_name == 'searchWeatherForecast':
-        print('searchWeatherForecast')
-        sentence += 'the weather '
-    elif intent_message.intent.intent_name == 'Tengu:searchWeatherForecast':
-        print('Tengu:searchWeatherForecastTemperatureBlaBla')
-        sentence += 'the temperature '
-    elif intent_message.intent.intent_name == 'searchWeatherForecastCondition':
-        print('searchWeatherForecastCondition')
-        sentence += 'the weather condition '
-    elif intent_message.intent.intent_name == 'searchWeatherForecastItem':
-        print('searchWeatherForecastItem')
-        sentence += 'the weather '
+    if intent_message.intent.intent_name == 'addNumber':
+        sentence += 'pas de Tengu '
+    elif intent_message.intent.intent_name == 'Tengu:addNumber':
+        sentence += 'avec Tengu '
     else:
         return
 
-    forecast_country_slot = intent_message.slots.forecast_country.first()
-    forecast_locality_slot = intent_message.slots.forecast_locality.first()
-    forecast_start_datetime_slot = intent_message.slots.forecast_start_datetime
+    nb_1 = intent_message.slots.nb_1.first()
+    nb_2 = intent_message.slots.nb_2.first()
 
-    if forecast_locality_slot is not None:
-        sentence += 'in ' + forecast_locality_slot.value
-    if forecast_country_slot is not None:
-        sentence += 'in ' + forecast_country_slot.value
-    if forecast_start_datetime_slot is not None and len(forecast_start_datetime_slot) > 0:
-        sentence += ' ' + forecast_start_datetime_slot[0].raw_value
+    if nb_1 is not None:
+        sentence += 'nb 1 : ' + nb_1.value
+    if nb_2 is not None:
+        sentence += 'nb 2 : ' + nb_2.value
 
     hermes.publish_end_session(intent_message.session_id, sentence)
 
